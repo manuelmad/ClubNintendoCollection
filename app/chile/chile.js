4f5109047162fc221e15712bec3870e2dcf1cfe6
Manuel Araujo <manuelmad1981@gmail.com>
2026-01-11 14:43:44 -0400

    Finished Chile

--- app/chile/chile.js
+++ app/chile/chile.js
@@ -1,31 +1,114 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function VenezuelaScreen() {
+const years = [
+  { year_date: 1992, year_edit: 1 },
+  { year_date: 1993, year_edit: 2 },
+  { year_date: 1994, year_edit: 3},
+  { year_date: 1995, year_edit: 4},
+  { year_date: 1996, year_edit: 5 },
+  { year_date: 1997, year_edit: 6 },
+  { year_date: 1998, year_edit: 7 },
+  { year_date: 1999, year_edit: 8 },
+  { year_date: 2000, year_edit: 9 },
+  { year_date: 2001, year_edit: 10},
+  { year_date: 2002, year_edit: 11},
+  { year_date: 2003, year_edit: 12},
+  { year_date: 2004, year_edit: 13},
+  { year_date: 2005, year_edit: 14},
+  { year_date: 2006, year_edit: 15},
+  { year_date: 2007, year_edit: 16},
+  { year_date: 2008, year_edit: 17},
+  { year_date: 2009, year_edit: 18},
+  { year_date: 2010, year_edit: 19},
+  { year_date: 2011, year_edit: 20},
+  { year_date: 2012, year_edit: 21},
+  { year_date: 2013, year_edit: 22},
+  { year_date: 2014, year_edit: 23},
+  { year_date: 2015, year_edit: 24},
+  { year_date: 2016, year_edit: 25},
+  { year_date: 2017, year_edit: 26},
+  { year_date: 2018, year_edit: 27},
+  { year_date: 2019, year_edit: 28}
+]
+
+const country = "Chile";
+
+export default function ChileScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Colección Chile</Text>
-      <Text style={styles.text}>(Próximamente)</Text>
-      <Link style={styles.link} href="/">Volver al Inicio</Link>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Colección Chile</Text>
+      <ScrollView>
+        <View >
+        <View style={styles.grid}>
+            {years.map((year, index) => (
+              <Link key={index} href={{ pathname: "/chile/covers/covers", params: { year: year.year_edit, country: country} }} asChild>
+                <Pressable style={styles.card}>
+                  <Text style={styles.text}>Año {year.year_edit}</Text>
+                  <Text style={styles.text}>({year.year_date})</Text>
+                </Pressable>
+              </Link>
+            ))}
+            <Link href={{ pathname: "/chile/covers/covers",  params: {country: country} }} asChild>
+                <Pressable style={styles.card}>
+                  <Text style={styles.text}>Especiales</Text>
+                </Pressable>
+              </Link>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Link href="/" style={styles.link}>Volver al Inicio</Link>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+    // position: "fixed",
+    // top: 0,
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 15,
+    maxWidth: 300, // Restringe el ancho para forzar 2 columnas (aprox 120*2 + gap)
+  },
+  card: {
+    width: 120,
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fe0000",
+    borderRadius: 10,
   },
   text: {
-    color: '#fff',
-    fontSize: 24,
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
-  link:{
-    marginTop: 20,
-    color: '#1e90ff',
-    fontSize: 18,
-  }
+    footer: {
+    padding: 20,
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+  link: {
+    color: "#007AFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
+
---
+++ app/chile/covers/covers.js
@@ -0,0 +1,255 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Link, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import inventory from '../inventory';
+
+// Cargar dinámicamente todas las imágenes .jpg dentro de la carpeta images
+// Esto funciona gracias a Metro (el bundler de React Native/Expo)
+const images = require.context('../images', true, /\.jpg$/);
+
+export default function CoversScreen() {
+  const { year } = useLocalSearchParams();
+  const { country } = useLocalSearchParams();
+  const [data, setData] = useState(inventory);
+
+  useEffect(() => {
+    const loadData = async () => {
+      // Esta línea la uso para limpiar la base de datos almacenada en AsyncStorage durante las pruebas
+      //await AsyncStorage.removeItem('chileDB')
+      const storedDb = await AsyncStorage.getItem("chileDB");
+      if (storedDb) {
+        setData(JSON.parse(storedDb));
+      }
+    };
+    loadData();
+  }, []);
+
+  // Filtrar el inventario por el año seleccionado
+  const filteredData = data.filter(item => item["YEAR EDIT"] == year && item["OVERALL NUMBER"] !== "SPECIAL" && item["OVERALL NUMBER"] !== "SPECIAL2");
+
+ // Filtrar el inventario por especiales
+  const specialsFilteredData = data.filter(item => item["OVERALL NUMBER"] == "SPECIAL" || item["OVERALL NUMBER"] == "SPECIAL2");
+
+
+  const renderItem = ({ item, index }) => {
+    // Asumimos que las imágenes están enumeradas 1.jpg, 2.jpg... coincidiendo con el orden del inventario filtrado
+    const imageNumber = index + 1;
+    let imageSource = null;
+
+    try {
+      // Construimos la ruta relativa para require.context: ./year_edit/number.jpg
+      // Nota: require.context usa rutas relativas desde este archivo
+      const imagePath = `./${year}/${imageNumber}.jpg`;
+      imageSource = images(imagePath);
+    } catch (error) {
+      console.log(`Imagen no encontrada: ${year}/${imageNumber}.jpg`);
+    }
+
+    return (
+      <View style={[styles.card, item["OWNED"] === "NO" ? {backgroundColor: "#f3e1e3"} : { backgroundColor: "#c6eadf" }]}>
+        {imageSource ? (
+          <Image source={imageSource} style={styles.image} resizeMode="contain" />
+        ) : (
+          <View style={[styles.image, styles.placeholder]}>
+            <Text style={styles.placeholderText}>No Image</Text>
+          </View>
+        )}
+        <Text style={styles.coverText} numberOfLines={2}>#{item["OVERALL NUMBER"]}</Text>
+        <Text style={styles.coverText} numberOfLines={2}>Año {item["YEAR EDIT"]} No. {item["YEAR NUMBER"]}</Text>
+        <Text style={styles.coverText} numberOfLines={2}>{item["MONTH"]}-{item["YEAR DATE"]}</Text>
+        <Text style={styles.coverText} numberOfLines={4}>Poster: {item["POSTER THEME"] == '-' ? 'No incluye' : item["POSTER THEME"]}</Text>
+        <Pressable style={[styles.button, item["OWNED"] === "NO" ? {backgroundColor: "#f0394d"} : { backgroundColor: "#006845" }]}
+          onPress={
+            async () => {
+              const newData = data.map((el) => {
+                if (el["OVERALL NUMBER"] === item["OVERALL NUMBER"]) {
+                  return { ...el, "OWNED": el["OWNED"] === "NO" ? "YES" : "NO" };
+                }
+                
+                return el;
+              });
+              setData(newData);
+              await AsyncStorage.setItem("chileDB", JSON.stringify(newData));
+            }
+          }>
+          <Text style={styles.buttonText}>{item["OWNED"] === "NO" ? "NO" : "SÍ"}</Text>
+        </Pressable>
+      </View>
+    );
+  };
+
+  const renderSpecialItem = ({ item }) => {
+    // Asumimos que las imágenes están enumeradas 1.jpg, 2.jpg... coincidiendo con el orden del inventario filtrado
+    const imageNumber = item["OVERALL NUMBER"] == "SPECIAL" ? "SPECIAL" : "SPECIAL2" ;
+    let imageSource = null;
+
+
+    try {
+    // Construimos la ruta relativa para require.context: ./year_edit/number.jpg
+    // Nota: require.context usa rutas relativas desde este archivo
+    const imagePath = `./${item["YEAR EDIT"]}/${imageNumber}.jpg`;
+    imageSource = images(imagePath);
+    } catch (error) {
+      console.log(`Imagen no encontrada: ${item["YEAR EDIT"]}/${imageNumber}.jpg`);
+    }
+
+    return (
+      <View style={[styles.specialCard, item["OWNED"] === "NO" ? {backgroundColor: "#f3e1e3"} : { backgroundColor: "#c6eadf" }]}>
+        {imageSource ? (
+          <Image source={imageSource} style={styles.image} resizeMode="contain" />
+        ) : (
+          <View style={[styles.image, styles.placeholder]}>
+            <Text style={styles.placeholderText}>No Image</Text>
+          </View>
+        )}
+        <Text style={styles.coverText} numberOfLines={2}>{item["MONTH"]}-{item["YEAR DATE"]}</Text>
+        <Text style={styles.coverText} numberOfLines={4}>Poster: {item["POSTER THEME"] == '-' ? 'No incluye' : item["POSTER THEME"]}</Text>
+         <Pressable style={[styles.button, item["OWNED"] === "NO" ? {backgroundColor: "#f0394d"} : { backgroundColor: "#006845" }]}
+          onPress={
+            async () => {
+              const newData = data.map((el) => {
+                if (el["COVER"] === item["COVER"]) {
+                  return { ...el, "OWNED": el["OWNED"] === "NO" ? "YES" : "NO" };
+                }
+                
+                return el;
+              });
+              setData(newData);
+              await AsyncStorage.setItem("chileDB", JSON.stringify(newData));
+            }
+          }>
+          <Text style={styles.buttonText}>{item["OWNED"] === "NO" ? "NO" : "SÍ"}</Text>
+        </Pressable>
+      </View>
+    );
+
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Revistas {year ? `Año ${year}` : 'Especiales'} {country}</Text>
+      </View>
+      {year ? 
+          <FlatList
+            data={filteredData}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={2}
+            contentContainerStyle={styles.grid}
+            columnWrapperStyle={styles.row}
+          /> :
+          <FlatList
+            data={specialsFilteredData}
+            renderItem={renderSpecialItem}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={2}
+            contentContainerStyle={styles.grid}
+            columnWrapperStyle={styles.row}
+          />
+      }
+     
+
+      <View style={styles.footer}>
+        <Link href="/chile/chile" style={styles.link}>Volver a Años</Link>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    padding: 20,
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  grid: {
+    paddingHorizontal: 10,
+    paddingBottom: 20,
+  },
+  row: {
+    justifyContent: "space-around",
+    marginBottom: 15,
+  },
+  card: {
+    width: 160,
+    height: 370,
+    alignItems: "center",
+    // borderWidth: 1,
+    // backgroundColor: "#f9f9f9",
+    borderRadius: 10,
+    padding: 10,
+    // elevation: 2, // Sombra en Android
+    // shadowColor: "#f0394d", // Sombra en iOS
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 4,
+  },
+  specialCard: {
+    width: 160,
+    height: 350,
+    alignItems: "center",
+    // borderWidth: 1,
+    // backgroundColor: "#f9f9f9",
+    borderRadius: 10,
+    padding: 10,
+    // elevation: 2, // Sombra en Android
+    // shadowColor: "#000", // Sombra en iOS
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 4,
+  },
+  image: {
+    width: 140,
+    height: 200,
+    marginBottom: 10,
+  },
+  placeholder: {
+    backgroundColor: "#e0e0e0",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  placeholderText: {
+    color: "#888",
+  },
+  coverText: {
+    fontSize: 12,
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  footer: {
+    padding: 20,
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+  },
+  link: {
+    color: "#007AFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  button: {
+    width: 140,
+    height: 30,
+    marginTop: 10,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 3,
+    position: "absolute",
+    bottom: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
